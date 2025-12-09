3b5ee9be1c17bd32f7476f66fd0710159339ef6e
ACLIFF123 <aaronclifford26@gmail.com>
2025-12-09 12:44:36 +0000

    Feat: Add IAM role and permissions for ETL ingestion Lambda

--- terrafrom/lambda_iam.tf
+++ terrafrom/lambda_iam.tf
@@ -1,3 +1,27 @@
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "gamboge-etl-lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+
 resource "aws_lambda_function" "etl_ingestion" {
   function_name = "gamboge-etl-ingestion-lambda"
   role          = aws_iam_role.lambda_exec.arn
